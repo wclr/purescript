387814c6413f1c55bebad4aa697a82569d01d17b
Alex <alex.oshchepkov@gmail.com>
2023-03-08 02:03:14 +0500

    ide usages update, round one

--- tests/support/pscide/src/FindUsage.purs
+++ tests/support/pscide/src/FindUsage.purs
@@ -1,12 +1,32 @@
 module FindUsage where
 
-import FindUsage.Definition (usageId, ($%), Usage(..))
-import FindUsage.Reexport (toBeReexported)
+import FindUsage.Definition (($%), Usage(Used), usageId, class UsageTC)
+import FindUsage.Reexport2 (toBeReexported)
+-- import Usage both as non-qualified and qualified
+import FindUsage.Definition (Usage(..), usageId2) as D
+import FindUsage.Definition as D -- test for duplicated imports
 
-usagePatternMatch ∷ Usage → Usage
+
+usagePatternMatch :: Usage -> D.Usage
 usagePatternMatch x = case x of
-  Used _ → x
-  _ $% _ → x
+  D.Used _ -> (x :: D.Usage) -- typed value
+  _ $% _ -> x
 
-usageFn ∷ ∀ a. a → a
+-- without signature
+usedVal = Used 0
+
+usageFn :: ∀ a. a -> a
 usageFn = usageId toBeReexported
+
+-- assign to local
+usageId2 :: forall a. a -> a
+usageId2 = D.usageId2
+
+-- use local
+localUse :: Int
+localUse = usageId2 10
+
+newtype Find = Find Usage
+
+instance UsageTC Find where
+  use = Find
--- tests/support/pscide/src/FindUsage/Definition.purs
+++ tests/support/pscide/src/FindUsage/Definition.purs
@@ -1,4 +1,12 @@
-module FindUsage.Definition (Usage(..), ($%), usageId, toBeReexported) where
+module FindUsage.Definition
+  ( Usage(..)
+  , usageId
+  , usageId2
+  , class UsageTC
+  , use
+  , ($%)
+  , toBeReexported
+  ) where
 
 data Usage
   = Used Int
@@ -6,8 +14,24 @@ data Usage
 
 infixl 2 Usage as $%
 
-usageId ∷ ∀ a. a → a
+usageId :: ∀ a. a -> a
 usageId x = x
 
-toBeReexported ∷ ∀ a. a → a
+usageId2 :: ∀ a. a -> a
+usageId2 x = x
+
+toBeReexported :: ∀ a. a -> a
 toBeReexported = usageId
+
+localId :: ∀ a. a -> a
+localId x = x
+
+loc :: Int
+loc = localId 10
+
+type Usages = Array Usage
+
+infixl 2 type Usage as <$%>
+
+class UsageTC a where
+  use :: Usage -> a
