72955499697111bac267c3be6d1e3a9ed7eb3d99
Alex <alex.oshchepkov@gmail.com>
2023-12-26 19:26:08 +0500

    Ide update WIP

--- tests/Language/PureScript/Ide/CompletionSpec.hs
+++ tests/Language/PureScript/Ide/CompletionSpec.hs
@@ -32,9 +32,11 @@ spec = describe "Applying completion options" $ do
   it "keeps all matches if maxResults is not specified" $ do
     applyCompletionOptions (defaultCompletionOptions { coMaxResults = Nothing })
       (map fst matches) `shouldMatchList` matches
+
   it "keeps only the specified amount of maxResults" $ do
     applyCompletionOptions (defaultCompletionOptions { coMaxResults = Just 1 })
       (map fst matches) `shouldMatchList` take 1 matches
+
   it "groups reexports for a single identifier" $ do
     applyCompletionOptions (defaultCompletionOptions { coGroupReexports = True })
       reexportMatches `shouldBe` [(Match (mn "A", ideKind "Kind"), [mn "A", mn "B"])]
